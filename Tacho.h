46563566bd1b631cc9a31eb1cb60369d26b281f8
luisdmaco <luisdmaco@outlook.com>
2021-11-10 16:42:58 -0500

    clase tacho terminada

---
+++ Tacho.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Entidad.h"
+
+class Tacho : public Entidad {
+public:
+	Tacho(Bitmap^ img, int x, int y) {
+		width = img->Width;
+		height = img->Height;
+		this->x = x;
+		this->y = y;
+		dx = dy = 0;
+		IDx = 0;
+	}
+	~Tacho() {}
+	Rectangle AreaTacho() { return Rectangle(x - 20, y - 20, width + 40, height + 40); }
+	//inicio del area - hasta el final del baño + el area restante
+	void draw(Graphics^ g, Bitmap^ img) {
+		Rectangle corte = Rectangle(IDx * width, 0 * height, 64, 64);
+		g->DrawImage(img, AreaRectangle(), corte, GraphicsUnit::Pixel);
+	}
+	//para obtener y / x, las funciones están en entidad (herencia)
+};
