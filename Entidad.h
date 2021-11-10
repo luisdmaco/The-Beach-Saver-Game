8238e624f2c4a4986a8ef9643d916d1b8a0417c2
luisdmaco <luisdmaco@outlook.com>
2021-11-10 15:26:57 -0500

    clase monigote reescrita (leer comentarios)

--- Entidad.h
+++ Entidad.h
@@ -1,106 +1,142 @@
 #pragma once
 #include "Dependencias.h"
 
-public enum Direccion { right, left, nothing };
-
-class Entidad
-{
+class Entidad {
 protected:
 	int x, y;
-	int idx, idy;
 	int dx, dy;
-	int ancho, alto;
-	bool visible;
-	Direccion orientation;
-	int w, h;
+	//bool visible; //qué?
 	int width, height;
-	int limF, limC;
-	int fil, col;
-	int fracY, fracX;
-	int life;
-public: // utilicen el que más les convenga 
+	int IDx;
+public:
 	Entidad() {
-		visible = false;
-		dx = dy = 0;
-		orientation = Direccion::nothing;
-		width = height = 70;
-	}
-	Entidad(int px, int py, int pw, int ph) {
-		x = px; y = py;
-		w = pw; h = ph;
-		visible = false;
-		dx = dy = 0;
-		orientation = Direccion::nothing;
-		width = height = 70;
-	}
-	Entidad(int px, int py, int pw, int ph, int plimF, int plimC, int pfil, int pcol) {
-		x = px; y = py;
-		w = pw; h = ph;
-		limF = plimF;
-		limC = plimC;
-		fil = pfil;
-		col = pcol;
-		visible = false;
-		dx = dy = 0;
-		orientation = Direccion::nothing;
-		width = height = 70;
-	}
-	~Entidad(){}
-#pragma region Set_Get
-	// metodo get
-	int getx() { return x; }
-	int gety() { return y; }
-	int getw() { return w; }
-	int geth() { return h; }
-	int getlimF() { return limF; }
-	int getlimC() { return limC; }
-	int getcol() { return col; }
-	int getfil() { return fil; }
-	int getdx() { return dx; }
-	int getdy() { return dy; }
-	int getfracY() { return fracY; }
-	int getfracX() { return fracX; }
-	bool getvisible() { return visible; }
-	Rectangle getrec() { return Rectangle(x, y, width, height); }
-	// metodo set
-	void setx(int date) { x = date; }
-	void sety(int date) { y = date; }
-	void setw(int date) { w = date; }
-	void seth(int date) { h = date; }
-	void setlimF(int date) { limF = date; }
-	void setlimC(int date) { limC = date; }
-	void setcol(int date) { col = date; }
-	void setfil(int date) { fil = date; }
-	void setdx(int date) { dx = date; }
-	void setdy(int date) { dy = date; }
-	void setvisible(bool date) { visible = date; }
-	void setfracX(int date) { fracX = date; }
-	void setfracY(int date) { fracY = date; }
-#pragma endregion
+		x = y = 0; //sobreescribir en cada clase
+		dx = dy = 0; //sobreescribir en cada clase
+		x = y = 0; //sobreescribir en cada clase
+		width = height = 0; //sobreescribir en cada clase
+		IDx = 0; //sobreescribir en cada clase
 
-	/*void Desplazamiento(Direccion date)
-	{
-		dx = dy = 0;
-		if (orientation == right) { dx = (w / fracX); fil = 3; }
-		if (orientation == left) { dx = -(w / fracX); fil = 3; }
-	}*/
-	virtual void Move(Graphics^ g)
-	{
-		if (x + dx<1 || x + dx + w>g->VisibleClipBounds.Width) dx = dx * -1;
-		if (y + dy<1 || y + dy + h>g->VisibleClipBounds.Height) dy = dy * -1;
+	}
+	~Entidad() {}
+	virtual void draw(Graphics^ g, Bitmap^ img) { //draw default básico, en cada clase se va a sobreescribir
+		g->DrawImage(img, AreaRectangle());
+	}
+	virtual void move(Graphics^ g) { //move default básico, en cada clase se va a sobreescribir
 		x += dx;
 		y += dy;
 	}
-	virtual void Draw(Graphics^ g, Bitmap^ bmp)
+	Rectangle AreaRectangle() { return Rectangle(x, y, width, height); } 
+	//obtener area de cada sprite en cualquier lugar
+	Rectangle AreaNextRectangle() { return Rectangle(x + dx, y + dy, width, height); }
+	//obtener siguiente area de cada sprite en cualquier lugar (colisiones)
+#pragma region Set-Get-Functions
+	void setDx(int newdx)
 	{
-		w = bmp->Width / limC;
-		h = bmp->Height / limF;
-		Rectangle destiny = Rectangle(x, y, width, height);
-		Rectangle porcion = Rectangle(col * w, fil * h, w, h);
-		g->DrawImage(bmp, destiny, porcion, GraphicsUnit::Pixel);
-		col++;
-		if (col == limC) col = 0;
+		dx = newdx;
+	}
+	void setDy(int newdy)
+	{
+		dy = newdy;
 	}
 
-
+#pragma endregion
 };
+
+//class Entidad
+//{
+//protected:
+//	int x, y;
+//	int idx, idy;
+//	int dx, dy;
+//	bool visible;
+//	int orientation; //provisional
+//	int w, h;
+//	int width, height;
+//	int limF, limC;
+//	int fil, col;
+//	int fracY, fracX;
+//	int life;
+//public: // utilicen el que más les convenga 
+//	Entidad() {
+//		visible = false;
+//		dx = dy = 0;
+//		orientation = 0;
+//		width = height = 70;
+//	}
+//	Entidad(int px, int py, int pw, int ph) {
+//		x = px; y = py;
+//		w = pw; h = ph;
+//		visible = false;
+//		dx = dy = 0;
+//		orientation = 0;
+//		width = height = 70;
+//	}
+//	Entidad(int px, int py, int pw, int ph, int plimF, int plimC, int pfil, int pcol) {
+//		x = px; y = py;
+//		w = pw; h = ph;
+//		limF = plimF;
+//		limC = plimC;
+//		fil = pfil;
+//		col = pcol;
+//		visible = false;
+//		dx = dy = 0;
+//		orientation = 0;
+//		width = height = 70;
+//	}
+//	~Entidad(){}
+//#pragma region Set_Get
+//	// metodo get
+//	int getx() { return x; }
+//	int gety() { return y; }
+//	int getw() { return w; }
+//	int geth() { return h; }
+//	int getlimF() { return limF; }
+//	int getlimC() { return limC; }
+//	int getcol() { return col; }
+//	int getfil() { return fil; }
+//	int getdx() { return dx; }
+//	int getdy() { return dy; }
+//	int getfracY() { return fracY; }
+//	int getfracX() { return fracX; }
+//	bool getvisible() { return visible; }
+//	Rectangle getrec() { return Rectangle(x, y, width, height); }
+//	// metodo set
+//	void setx(int date) { x = date; }
+//	void sety(int date) { y = date; }
+//	void setw(int date) { w = date; }
+//	void seth(int date) { h = date; }
+//	void setlimF(int date) { limF = date; }
+//	void setlimC(int date) { limC = date; }
+//	void setcol(int date) { col = date; }
+//	void setfil(int date) { fil = date; }
+//	void setdx(int date) { dx = date; }
+//	void setdy(int date) { dy = date; }
+//	void setvisible(bool date) { visible = date; }
+//	void setfracX(int date) { fracX = date; }
+//	void setfracY(int date) { fracY = date; }
+//#pragma endregion
+//
+//	/*void Desplazamiento(Direccion date)
+//	{
+//		dx = dy = 0;
+//		if (orientation == right) { dx = (w / fracX); fil = 3; }
+//		if (orientation == left) { dx = -(w / fracX); fil = 3; }
+//	}*/
+//	virtual void Move(Graphics^ g)
+//	{
+//		if (x + dx<1 || x + dx + w>g->VisibleClipBounds.Width) dx = dx * -1;
+//		if (y + dy<1 || y + dy + h>g->VisibleClipBounds.Height) dy = dy * -1;
+//		x += dx;
+//		y += dy;
+//	}
+//	virtual void Draw(Graphics^ g, Bitmap^ bmp)
+//	{
+//		w = bmp->Width / limC;
+//		h = bmp->Height / limF;
+//		Rectangle destiny = Rectangle(x, y, width, height);
+//		Rectangle porcion = Rectangle(col * w, fil * h, w, h);
+//		g->DrawImage(bmp, destiny, porcion, GraphicsUnit::Pixel);
+//		col++;
+//		if (col == limC) col = 0;
+//	}
+//};
--- monigote.h
+++ monigote.h
@@ -1,70 +1,178 @@
 #pragma once
 #include "Entidad.h"
 
-class Humano : public Entidad
-{
+enum SpritePlayer {
+	Abajo,
+	Izquierda,
+	Derecha,
+	Arriba
+};
+
+class Humano : public Entidad {
+private:
+	SpritePlayer accion;
 public:
-	Humano(int x, int y)
-	{
-		this->x = x;
-		this->y = y;
-
-		//x = 100;
-		//y = 100;
-	   //comentario salvaje de prueba aparece xd xdxdxdxd
-
-		dx = 5;
-		dy = 5;
-		idx = 0;
-		idy = 0;
-	};
+	Humano(Bitmap^ img, int x, int y) {
+		height = img->Height / 4; //alto = alto de la imagen entre 4 (un sprite)
+		width = img->Width / 4; //ancho = ancho de la imagen entre 4 (un sprite)
+		dx = dy = 0; //default dx y dy = 0
+		accion = Abajo; //default acción mostrada en pantalla = sprite abajo
+		this->x = x; //set x en la posicion de pantalla deseada
+		this->y = y; //set y en la posicion de pantalla deseada
+	}
 	~Humano(){}
 
-	void Draw(Graphics^ g, Bitmap^ bmp) {
-		ancho = bmp->Width / 4;;
-		alto = bmp->Height / 4;;
-		Rectangle seccionmostrar = Rectangle(idx * ancho, idy * alto, ancho, alto);
-		Rectangle zoom = Rectangle(x, y, ancho, alto);
-		g->DrawImage(bmp, zoom, seccionmostrar, GraphicsUnit::Pixel);
-	}
-
-	void movimientoMonigote(Graphics^ g, Keys k) //keys es una clase que tiene mapeado todo el teclado
-	{
-		switch (k)
-		{
-		case Keys::Left: //flecha izquierda
-			if (x > 0)
+	void draw(Graphics^ g, Bitmap^ img) {
+		Rectangle corte = Rectangle(IDx * width, accion * height, 64, 64);
+		//corte de la imagen
+		g->DrawImage(img, AreaRectangle(), corte, GraphicsUnit::Pixel);
+		if (dx != 0 || dy != 0) { //si se mueve
+			switch (accion) //depende de la acción que se muestra movimiento del sprite
 			{
-				idy = 1;
-				x -= dx;
-			} break;
-
-		case Keys::Up: //flecha arriba
-
-			if (y > 50)
-			{
-				idy = 3;
-				y -= dy;
-
-			}break;
-		case Keys::Right: //flecha derecha
-			if (x + ancho * 1 < g->VisibleClipBounds.Width)
-			{
-				idy = 2;
-				x += dx;
-			}break;
-		case Keys::Down: //flecha abajo
-			if (y + alto * 1 < g->VisibleClipBounds.Height)
-			{
-				idy = 0;
-				y += dy;
-			}break;
-		}
-
-		idx++;
-		if (idx > 3)
-		{
-			idx = 0;
+			case Abajo:
+				IDx = (IDx + 1) % 4; //el residuo de 4 entre 4 es 0, por lo que vuelve a 0 
+				break;               //y comienza a aumentar hasta 4 cada vez que se ejecuta
+			case Izquierda:
+				IDx = (IDx + 1) % 4;
+				break;
+			case Derecha:
+				IDx = (IDx + 1) % 4;
+				break;
+			case Arriba:
+				IDx = (IDx + 1) % 4;
+				break;
+			}
 		}
 	}
+	void move(Graphics^ g) {
+		if (x + dx > 0 && x + dx + width < g->VisibleClipBounds.Width) {
+			x += dx;
+		}  //si (x / y) es mayor a 0 y menor al limite visible del formulario
+		if (y + dy > 0 && y + dy + height < g->VisibleClipBounds.Height) {
+			y += dy;
+		}
+	}
+	void MovimientoMonigote(bool action, Keys key, Graphics^ g) { //ESTA FUNCIÓN SE MOVERÁ A CONTROLLER
+		int value = 5; //value == dx / dy
+		if (action == true)
+		{
+			switch (key)
+			{
+			case Keys::Left: //flecha izquierda
+				setDx(-value); //nuevo valor de dx;
+				setAccion(Izquierda);
+				move(g); //accion derecha (movimiento y muestra)
+				break; //mover
+				//así para todos
+			case Keys::Up: //flecha arriba
+				setDy(-value);
+				setAccion(Arriba);
+				move(g);
+				break;
+			case Keys::Right: //flecha derecha
+				setDx(value); 
+				setAccion(Derecha); 
+				move(g); 
+				break;
+			case Keys::Down: //flecha abajo
+				setDy(value);
+				setAccion(Abajo);
+				move(g);
+				break;
+			}
+		}
+		else //si bool es falso (implementado en keyup)
+		{
+			switch (key)
+			{
+			case Keys::Left:
+				setDx(0); //dx / dy == 0
+				break;
+			case Keys::Up:
+				setDy(0);
+				break;
+			case Keys::Right:
+				setDx(0);
+				break;
+			case Keys::Down:
+				setDy(0);
+				break;
+			}
+		}
+	}
+#pragma region Set-Get-Functions
+	void setAccion(SpritePlayer value) {
+		accion = value;
+	}
+#pragma endregion
 };
+
+//antigua función (inservible)
+
+//class Humano : public Entidad
+//{
+//public:
+//	Humano(int x, int y)
+//	{
+//		this->x = x;
+//		this->y = y;
+//
+//		//x = 100;
+//		//y = 100;
+//	   //comentario salvaje de prueba aparece xd xdxdxdxd
+//
+//		dx = 5;
+//		dy = 5;
+//		idx = 0;
+//		idy = 0;
+//	};
+//	~Humano(){}
+//
+//	void Draw(Graphics^ g, Bitmap^ bmp) {
+//		ancho = bmp->Width / 4;;
+//		alto = bmp->Height / 4;;
+//		Rectangle seccionmostrar = Rectangle(idx * ancho, idy * alto, ancho, alto);
+//		Rectangle zoom = Rectangle(x, y, ancho, alto);
+//		g->DrawImage(bmp, zoom, seccionmostrar, GraphicsUnit::Pixel);
+//	}
+//
+//	void movimientoMonigote(Graphics^ g, Keys k) //keys es una clase que tiene mapeado todo el teclado
+//	{
+//		switch (k)
+//		{
+//		case Keys::Left: //flecha izquierda
+//			if (x > 0)
+//			{
+//				idy = 1;
+//				x -= dx;
+//			} break;
+//
+//		case Keys::Up: //flecha arriba
+//
+//			if (y > 50)
+//			{
+//				idy = 3;
+//				y -= dy;
+//
+//			}break;
+//		case Keys::Right: //flecha derecha
+//			if (x + ancho * 1 < g->VisibleClipBounds.Width)
+//			{
+//				idy = 2;
+//				x += dx;
+//			}break;
+//		case Keys::Down: //flecha abajo
+//			if (y + alto * 1 < g->VisibleClipBounds.Height)
+//			{
+//				idy = 0;
+//				y += dy;
+//			}break;
+//		}
+//
+//		idx++;
+//		if (idx > 3)
+//		{
+//			idx = 0;
+//		}
+//	}
+//};
